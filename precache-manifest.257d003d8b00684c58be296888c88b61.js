853c7e2d16f2603f93c7d6d9cb0b42c23b898185
Lucas Duarte <l.duarte.mk@gmail.com>
2020-06-20 13:31:49 -0300

    Updates

---
+++ precache-manifest.257d003d8b00684c58be296888c88b61.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428655d519472934da5c42c44b023b9c",
+    "url": "/qagil-hotsite/index.html"
+  },
+  {
+    "revision": "ce7631e5a224c657d527",
+    "url": "/qagil-hotsite/static/css/main.313ef558.chunk.css"
+  },
+  {
+    "revision": "36c014de650288051b94",
+    "url": "/qagil-hotsite/static/js/2.8267a960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qagil-hotsite/static/js/2.8267a960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7631e5a224c657d527",
+    "url": "/qagil-hotsite/static/js/main.71092ef8.chunk.js"
+  },
+  {
+    "revision": "660ec9792a94f588df25",
+    "url": "/qagil-hotsite/static/js/runtime-main.e844d419.js"
+  },
+  {
+    "revision": "10a5565cb0499e521580fa2fb443ff40",
+    "url": "/qagil-hotsite/static/media/PartysUser.10a5565c.png"
+  },
+  {
+    "revision": "712eb1627e1d80343ddb12b03b53b24f",
+    "url": "/qagil-hotsite/static/media/balloon.712eb162.png"
+  },
+  {
+    "revision": "14cabafbd67dc88701655424794fdd07",
+    "url": "/qagil-hotsite/static/media/barmanConcluido.14cabafb.png"
+  },
+  {
+    "revision": "4be6201bb113ff24ba82e5d4da90e43b",
+    "url": "/qagil-hotsite/static/media/carrinhoUser.4be6201b.png"
+  },
+  {
+    "revision": "769159f31787e62cb18340460a5dd528",
+    "url": "/qagil-hotsite/static/media/compraCloncluidaBarman.769159f3.png"
+  },
+  {
+    "revision": "c93f7daa4309e7d353e0897417e8b5d5",
+    "url": "/qagil-hotsite/static/media/compraConcluidaUser.c93f7daa.png"
+  },
+  {
+    "revision": "922459e7c8e2f208febc65929ec6e5ea",
+    "url": "/qagil-hotsite/static/media/createevent.922459e7.png"
+  },
+  {
+    "revision": "2647927d2e5ebdfa5b3a289fdc73110f",
+    "url": "/qagil-hotsite/static/media/editprofile.2647927d.png"
+  },
+  {
+    "revision": "1cd1c9141dbdd3c1cc58a80d8ac23120",
+    "url": "/qagil-hotsite/static/media/eventos.1cd1c914.png"
+  },
+  {
+    "revision": "403d13fd9deb0095cc718fb78b17c5db",
+    "url": "/qagil-hotsite/static/media/exampleCreateEvent.403d13fd.png"
+  },
+  {
+    "revision": "52ecc6f31813ce1155a45a7a42611bbb",
+    "url": "/qagil-hotsite/static/media/exampleCreateMenu.52ecc6f3.png"
+  },
+  {
+    "revision": "c41699ffc6f53e9f97f9b9eb4adf8634",
+    "url": "/qagil-hotsite/static/media/home.c41699ff.png"
+  },
+  {
+    "revision": "bc252e4f69cbb614bcfd61480696a7d5",
+    "url": "/qagil-hotsite/static/media/homeBarman.bc252e4f.png"
+  },
+  {
+    "revision": "3df05cbecf97defaca2401c777cbbbff",
+    "url": "/qagil-hotsite/static/media/infoProdutoUser.3df05cbe.png"
+  },
+  {
+    "revision": "5ce28b49d0933fc3838511d2e6194abc",
+    "url": "/qagil-hotsite/static/media/leituraQRCODEBarman.5ce28b49.png"
+  },
+  {
+    "revision": "e81a3a1783ba85c8583a40385af45ea3",
+    "url": "/qagil-hotsite/static/media/lendoQRCODEExample.e81a3a17.png"
+  },
+  {
+    "revision": "abdb14c7b01d4099fa7dde38bf4031b9",
+    "url": "/qagil-hotsite/static/media/login.abdb14c7.png"
+  },
+  {
+    "revision": "b9c5d63e2d22355f929d35e8d7d28291",
+    "url": "/qagil-hotsite/static/media/loginUser.b9c5d63e.png"
+  },
+  {
+    "revision": "c845baefe4e502b83cd22505fb0b61e8",
+    "url": "/qagil-hotsite/static/media/logo.c845baef.png"
+  },
+  {
+    "revision": "ffa2fc66ea4abfd7c82cca2e8d37fb18",
+    "url": "/qagil-hotsite/static/media/mostrandoQRCODEExample.ffa2fc66.png"
+  },
+  {
+    "revision": "9dc744dcba69a28a114d88c452bbca8a",
+    "url": "/qagil-hotsite/static/media/outdoor.9dc744dc.png"
+  },
+  {
+    "revision": "ec59ae4901de4c174404eaeab3e1f818",
+    "url": "/qagil-hotsite/static/media/party.ec59ae49.png"
+  },
+  {
+    "revision": "064e38dbb799fefecce785d830c85051",
+    "url": "/qagil-hotsite/static/media/qrcodeUser.064e38db.png"
+  },
+  {
+    "revision": "e3bf247d44c5fb105693261dedfc0de3",
+    "url": "/qagil-hotsite/static/media/seta.e3bf247d.png"
+  },
+  {
+    "revision": "bb22d568d5804b6e5c3c5cb05ae8ca8d",
+    "url": "/qagil-hotsite/static/media/userConcluido.bb22d568.png"
+  }
+]);
